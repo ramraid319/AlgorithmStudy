cb2e495a6ea38a9faaf6e3ac7d9475c86593e9cf
RamRaid <71435969+ramraid319@users.noreply.github.com>
2024-04-01 00:39:59 +0900

    update p10773.cpp

--- junpyo/week1/stack/p10773.cpp
+++ junpyo/week1/stack/p10773.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
@@ -7,5 +7,20 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    int k, sum=0;
+    cin >> k;
+    vector<int> v;
+
+    for (int i = 0; i < k; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        if (tmp == 0)
+            v.pop_back();
+        else
+            v.push_back(tmp);
+    }
+    for(auto it: v) sum+=it;
+    cout << sum;
     return 0;
 }
