6e8d7909207bd9878d1b177303f7c4e47b811cf5
RamRaid <71435969+ramraid319@users.noreply.github.com>
2024-04-01 14:43:01 +0900

    update p1874

--- junpyo/week1/stack/p1874.cpp
+++ junpyo/week1/stack/p1874.cpp
@@ -1,54 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n=0, tmp=0, size=0;
-    vector<int> vec;
-    vector<char> res;
-    bool isPos = true;
-    cin >> n;
-    int idx=1;
-
-    while (n--)
-    {
-        if(vec.empty())
-        {
-            vec.push_back(idx++);
-            res.push_back('+');
-        }
-        if(tmp==*prev(vec.end()))
-        {
-            vec.pop_back();
-            res.push_back('-');
-        }
-        cin >> tmp;
-        if(!vec.empty())
-        {
-            if(tmp < *prev(vec.end()))
-            {
-                isPos=false;
-                break;
-            }
-
-            while (tmp>*prev(vec.end()))
-            {
-                vec.push_back(idx++);
-                res.push_back('+');
-            }            
-
-        }
-
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  stack<int> S;
+  int cnt = 1;
+  string ans;
+  while (n--) {
+    int t;
+    cin >> t;
+    while (cnt <= t) {
+      S.push(cnt++);
+      ans += "+\n";
     }
-    
-    
-    if (isPos)
-        for(auto it: res) cout << it << endl;
-    else if(!isPos) cout << "NO";
-
-    return 0;
+    if (S.top() != t) {
+      cout << "NO\n";
+      return 0;
+    }
+    S.pop();
+    ans += "-\n";
+  }
+  cout << ans;
 }
